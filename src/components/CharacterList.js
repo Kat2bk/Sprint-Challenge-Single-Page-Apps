9643a982f1d0848db324ccd3361ac8d2811e116c
unknown <Kat2bk@gmail.com>
2020-01-31 08:43:48 -0800

    added axios code, imported component to app.js, will begin to map over characters

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,11 +1,21 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function CharacterList() {
-  // TODO: Add useState to track data from useEffect
+ const [data, setData] = useState([]);
 
   useEffect(() => {
-    // TODO: Add API Request here - must run in `useEffect`
-    //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    const getCharacters = () => {
+    axios.get(`https://rickandmortyapi.com/api/character/`)
+    .then(response => {
+      console.log("axios call", response);
+      setData(response.data);
+    })
+    .catch(error => {
+      console.log("server error", error);
+    })
+    }
+    getCharacters();
   }, []);
 
   return (
